40946430f665cb81e8d26dad521f95a83adeae02
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-05 18:59:06 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT p.category, SUM(pl.amount) FROM projects p INNER JOIN pledges pl "+
+"SELECT p.category, pl.amount FROM projects p INNER JOIN pledges pl "+
 "WHERE p.category = 'music'"
 end
 
