daa3129aad8d48d36799baf9e13d347f48064823
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-05 17:29:13 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,19 @@
+CREATE TABLE projects
+(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  category TEXT,
+  funding_goal NUMBER,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE projects
+(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  category TEXT,
+  fundingGoal NUMBER,
+  startDate DATE,
+  endDate DATE
+);
