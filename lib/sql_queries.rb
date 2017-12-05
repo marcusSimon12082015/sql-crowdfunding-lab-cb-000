448754d16852497334c88e398f154f1d436d9894
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-05 18:00:08 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -12,7 +12,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT u.name, u.age, pl.amount FROM users AS s INNER JOIN pledges AS pl ON u.id = pl.user_id "+
+"SELECT u.name, u.age, pl.amount FROM users AS u INNER JOIN pledges AS pl ON u.id = pl.user_id "+
 "GROUP BY u.name ORDER BY u.name"
 end
 
