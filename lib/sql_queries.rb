1489ee47bfd3ad85b653c804c770bdc201d25aea
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-05 18:51:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT u.name, SUM(pl.amount) FROM users u INNER JOIN pledges pl ON u.id = pl.user_id "+
-"GROUP BY u.name ORDER BY pl.amount, u.name"
+"GROUP BY u.name ORDER BY SUM(pl.amount), u.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
