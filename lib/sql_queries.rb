118025b4be94d6df4fad819794e1bc0e23efad18
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-05 19:08:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,6 +32,6 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT p.category, SUM(pl.amount) FROM project p INNER JOIN pledges pl ON p.id = pl.project_id "+
+"SELECT p.category, SUM(pl.amount) FROM projects p INNER JOIN pledges pl ON p.id = pl.project_id "+
 "WHERE p.category = 'books'"
 end
