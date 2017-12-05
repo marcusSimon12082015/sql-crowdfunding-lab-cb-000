0a3d7d42e296186911f3881ecb378f73a608746c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-05 18:29:18 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,8 +17,8 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT p.title, MAX(pl.amount) FROM projects p INNER JOIN pledges pl ON p.id = pl.project_id "+
-"WHERE pl.amount = p.funding_goal"
+"SELECT p.title, (SUM(pl.amount) - p.funding_goal) AS razlika FROM projects p INNER JOIN pledges pl ON p.id = pl.project_id "+
+"GROUP BY p.title HAVING SUM(pl.amount) => p.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
