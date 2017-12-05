281bb7ad295cc80102259208ea6c2c589e64cd93
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-05 18:14:48 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT p.title, pl.amount FROM projects p INNER JOIN pledges pl ON p.id = pl.project_id "+
+"SELECT p.title, MAX(pl.amount) FROM projects p INNER JOIN pledges pl ON p.id = pl.project_id "+
 "WHERE pl.amount = p.funding_goal"
 end
 
